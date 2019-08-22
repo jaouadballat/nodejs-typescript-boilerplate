1e61864ad248e7b4379d0b4747be6cf99e27a14c
jaouadballat <jaouad.ballat@gmail.com>
2019-08-22 11:34:06 +0100

    Login & register function for ahtenticated controller

--- src/controllers/user/authenticated.controller.ts
+++ src/controllers/user/authenticated.controller.ts
@@ -5,21 +5,25 @@ import * as express from 'express'
 import IUser from '../user/user.interface';
 import HttpException from '../../exceptions/HttpException';
 import UserWithThatEmailAlreadyExist from "../../exceptions/UserWithThatEmailAlreadyExist";
+import WrongCredentialException from "../../exceptions/WrongCredentialException";
 
 require('dotenv').config()
 
 export default class AuthenticatedController extends Controller {
+    
 
-    private User = UserModel;
+    private User;
 
     constructor() {
         super();
         this.path = 'auth';
+        this.User = UserModel;
         this.initializeRoutes();
     }
 
     private initializeRoutes() {
-        this.router.get(`${this.path}/signup`, this.register);
+        this.router.post(`${this.path}/signup`, this.register);
+        this.router.post(`${this.path}/signin`, this.login);
     }
 
     private register(request: express.Request, response: express.Response, next: express.NextFunction) {
@@ -42,5 +46,21 @@ export default class AuthenticatedController extends Controller {
 
         });
     }
-    
+
+    private login(request: express.Request, response: express.Response, next: express.NextFunction) {
+        let user: IUser = request.body;
+        this.User.findOne({ email: user.email }, (err, userLogedIn) => {
+            if(err) return next(new HttpException(500, err));
+            if(!user) return next(new WrongCredentialException());
+            bcrypt.compare(user.password, userLogedIn.password, (err, res) => {
+                if(!res) return next(new WrongCredentialException())
+                return response.send({
+                    ...userLogedIn,
+                    password: ""
+                });
+            });
+
+        })
+    }
+
 }
--- src/server.ts
+++ src/server.ts
@@ -1,9 +1,11 @@
 import App from "./app";
 
 import PostController from './controllers/post/post.controller'
+import AuthenticatedController from "./controllers/user/authenticated.controller";
 
 const app = new App([
-    PostController.handle()
+    PostController.handle(),
+    AuthenticatedController.handle()
 ], 3000)
 
 app.listen();
