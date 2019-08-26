d288746e487a121d2872b2fec8a664494079fc30
jaouadballat <jaouad.ballat@gmail.com>
2019-08-26 10:25:18 +0100

    refactor controller

--- src/controllers/controller.ts
+++ src/controllers/controller.ts
@@ -1,14 +1,31 @@
 import * as express from 'express';
+import HttpException from '../exceptions/HttpException';
+
 
 require('dotenv').config()
 
 export default class Controller {
 
     protected path: string = '';
-    protected router = express.Router() 
+    protected router = express.Router();
+    protected model; 
 
  
     public static handle() {
         return new this();
     } 
+
+    protected getAll = (request: express.Request, response: express.Response, next: express.NextFunction) => {
+        this.model.find({}, (err, data) => {
+            if(err) return next(new HttpException(500, err));
+            return response.send(data)
+        });
+    }
+
+    protected findById = (request: express.Request, response: express.Response, next: express.NextFunction) => {
+        this.model.findOneById(request.param.bind, (err, data) => {
+            if(err) return next(new HttpException(500, err));
+            return response.send(data)
+        })
+    }
 }
