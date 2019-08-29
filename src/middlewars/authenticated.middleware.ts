09cfdcdf172d9f132c1b8a373ef333b4f99f0e84
jaouadballat <jaouad.ballat@gmail.com>
2019-08-29 15:47:13 +0100

    wrong token

--- src/middlewars/authenticated.middleware.ts
+++ src/middlewars/authenticated.middleware.ts
@@ -1,11 +1,29 @@
 import * as express from 'express';
+import * as jwt from 'jsonwebtoken';
 import MissingTokenException from '../exceptions/MissingTokenException';
+import userModel from '../Model/user.model';
+import HttpException from '../exceptions/HttpException';
+import WrongAuthenticationTokenException from '../exceptions/WrongAuthenticationTokenException';
+
+require('dotenv').config()
 
 
 function authenticatedMiddleware(request: express.Request, response: express.Response, next: express.NextFunction) {
     const cookie = request.cookies;
     if(cookie && cookie.token) {
-
+        try {
+            let userId = jwt.verify(cookie.token, process.env.PRIVATE_TOKEN_KEY);
+            userModel.findById(userId._id, (err, user) => {
+                if(err) return next(new HttpException(500, err));
+                if(user) {
+                    //request.user = user; 
+                } else {
+                    return next(new WrongAuthenticationTokenException())
+                }
+            })
+        } catch (error) {
+            next(new MissingTokenException())
+        }
     } else {
         return next(new MissingTokenException())
     }
